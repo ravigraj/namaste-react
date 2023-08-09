c903426968dcecf9e02c8ce0d66b564805880090
Ravi G <ravigraj21@gmail.com>
2023-08-09 17:10:18 +0530

    Episode - 06 Explore the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,82 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  let [resListState, setResListState] = useState(resList); // state variable of React
-  return (
+  const [resListState, setResListState] = useState([]); // state variable of React
+
+  const [resFilterListState, setResFilterListState] = useState([]); // state variable of React
+
+  const [searchText, setSearchText] = useState([]); // state variable of React
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9713697&lng=77.755937&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setResListState(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setResFilterListState(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  //   if (resListState.length === 0) {
+  //     return <Shimmer />;
+  //   }
+
+  return resListState.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-input"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            type="button"
+            className="search-button"
+            onClick={() => {
+              const filteredResList = resListState.filter((resListItem) =>
+                resListItem.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setResFilterListState(filteredResList);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             const filteredResList = resListState.filter(
-              (resListItem) => resListItem.data.avgRating > 4
+              (resListItem) => resListItem.info.avgRating > 4
             );
-            setResListState(filteredResList);
+            setResFilterListState(filteredResList);
           }}
         >
           Top Restaurant
         </button>
       </div>
       <div className="restaurant-container">
-        {resListState.map((resListItem) => (
-          <RestaurantCard key={resListItem.data.id} resData={resListItem} />
+        {resFilterListState.map((resListItem) => (
+          <RestaurantCard key={resListItem.info.id} resData={resListItem} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,19 +13,19 @@ const RestaurantCard = (props) => {
     costForTwo,
     deliveryTime,
     cloudinaryImageId,
-  } = resData?.data;
+  } = resData?.info;
 
   return (
     <div className="restaurant-card" style={styleCard}>
       <img
         className="res-logo"
         alt="res-logo"
-        src={CDN_URL + resData.data.cloudinaryImageId}
+        src={CDN_URL + resData.info.cloudinaryImageId}
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{costForTwo}</h4>
       <h4>{deliveryTime} minutes</h4>
     </div>
   );
