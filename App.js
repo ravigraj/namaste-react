eb5d2493f6b0d1706bacbc6e3a8d9446cfa0de17
Ravi G <ravigraj21@gmail.com>
2025-04-18 20:47:54 +0530

    EP1 Inception

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement('div', { id: 'parent' });
+
+const heading = React.createElement(
+  'h1',
+  { id: 'heading' },
+  'Hello World from React!!!!'
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
+
+console.log(root);
