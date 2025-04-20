a13c2906f27f2e9f0c8f60873addddf2e4ddb129
Ravi G <ravigraj21@gmail.com>
2025-04-20 16:38:08 +0530

    EP3-Laying-the-Foundation

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement('div', { id: 'parent' });
+// React Element and JSX don't need to create with Capital letter.
+// React Element
+const heading = <h1>Namaste React with react Element</h1>;
 
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello World from React!!!!'
+//Using JSX;
+const jsxHead = <h1>Hello, Namaste React!</h1>;
+/************************** */
+
+// Any Normal Function can be used as Functional Component (but name should start with capital letter);
+const FunctionComp = function () {
+  return <h1>Namaste React with Normal Function </h1>;
+};
+
+//Arrow Functions with return
+const ArrowFunction = () => {
+  return <h1>Namaste React with Return </h1>;
+};
+
+//Arrow Functions with return
+const ArrowFunctionWithNoReturn = () => (
+  <h1 className="heading">Namaste React with No Return </h1>
 );
 
-console.log(heading);
+const NestedFunction = () => {
+  return (
+    <div className="heading">
+      {heading}
+      {jsxHead}
+      {`can't do this jsxHead()`}
+      <ArrowFunction />
+      {'RAVI GOVINDRAJ'}
+      {ArrowFunction()}
+      <ArrowFunctionWithNoReturn />
+      <ArrowFunction></ArrowFunction>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
-
-console.log(root);
+root.render(<NestedFunction />);
